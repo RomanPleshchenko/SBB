ddfd7f86bc0c55b3bb51d074969b815510280bd6
RomanPleshchenko <89313481052@mail.ru>
2017-04-04 19:26:28 +0300

    many errors

--- sbb-web/src/main/java/com/pleshchenko/sbb/model/Role.java
+++ sbb-web/src/main/java/com/pleshchenko/sbb/model/Role.java
@@ -33,31 +33,18 @@ public class Role implements Serializable{
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id.equals(role.id);
     }
 
     @Override
-    public boolean equals(Object obj) {
-
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof Role))
-            return false;
-        Role other = (Role) obj;
-
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public int hashCode() {
+        return id.hashCode();
     }
 
     @Override
--- sbb-web/src/main/java/com/pleshchenko/sbb/model/User.java
+++ sbb-web/src/main/java/com/pleshchenko/sbb/model/User.java
@@ -62,30 +62,18 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id.equals(user.id);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (!(obj instanceof User))
-            return false;
-        User other = (User) obj;
-
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public int hashCode() {
+        return id.hashCode();
     }
 
     @Override
